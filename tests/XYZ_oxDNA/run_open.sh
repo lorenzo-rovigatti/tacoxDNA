466d303b12862bddaaa785c5d8a93fa32c84b0bd
antonio-suma <antonio.suma@gmail.com>
2019-02-22 17:22:40 -0500

    change in XYZ->oxdna fixed open chain problem with first and last bead fixed same distance vector in computing p change example to include open chain

--- tests/XYZ_oxDNA/run_open.sh
+++ tests/XYZ_oxDNA/run_open.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-CORRECT_OUTPUT_CONF="correct_single_output.oxdna"
-CORRECT_OUTPUT_TOP="correct_single_output.top"
-OUTPUT_CONF="centerline.dat.oxdna"
-OUTPUT_TOP="centerline.dat.top"
+CORRECT_OUTPUT_CONF="correct_open_output.oxdna"
+CORRECT_OUTPUT_TOP="correct_open_output.top"
+OUTPUT_CONF="centerline_open.dat.oxdna"
+OUTPUT_TOP="centerline_open.dat.top"
 
-if [ ! -s centerline.dat ]
+if [ ! -s centerline_open.dat ]
 then
 	echo "Can't find input files. Are you sure you are in the right folder?"
 	exit 1
 fi
 
 rm $OUTPUT_CONF $OUTPUT_TOP 2> /dev/null
-python ../../src/XYZ_oxDNA.py centerline.dat -p 0.1 -q sequence.dat --ssDNA --open
+python ../../src/XYZ_oxDNA.py centerline_open.dat -p 0.1 --dsDNA --open --seed=1000
 (diff $CORRECT_OUTPUT_CONF $OUTPUT_CONF > /dev/null) && (diff $CORRECT_OUTPUT_TOP $OUTPUT_TOP > /dev/null)
 
 if [ $? -ne 0 ]
