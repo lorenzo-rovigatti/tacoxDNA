969ccf3b99114627790152f96a0940b1db86879f
dom-the-physicist <2dominikv@gmail.com>
2020-11-13 13:02:57 +0000

    The modified Lammps_oxdna script with the addition of adding strands

--- src/LAMMPS_oxDNA.py
+++ src/LAMMPS_oxDNA.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     basename = os.path.basename(sys.argv[1])
     topology_file = basename + ".top"
     configuration_file = basename + ".oxdna"
-    system.print_lorenzo_output(configuration_file, topology_file)
+    #system.print_lorenzo_output(configuration_file, topology_file)
 
     if len(sys.argv) == 3:
 
@@ -112,23 +112,61 @@ if __name__ == '__main__':
               line = f.readline()
               N = conf.natoms
 
-              conf.xyz = np.zeros((N, 3), dtype=float)
-              conf.v   = np.zeros((N, 3), dtype=float)
-
               for n in range(conf.natoms):
 
-                  position = np.float32(line.split()[3:6])
-                  conf.xyz = position
+                  index = int(line.split()[0])-1
+# Position
+                  cm = np.float32(line.split()[3:6])
+                  conf.xyz[index,:] = cm
+# Velocity 
                   velocity = np.float32(line.split()[6:9])
-                  conf.v = velocity
-
+                  conf.v[index,:] = velocity
+# Quaternions 
+                  quaternions = np.float32(line.split()[9:13])
+                  conf.ellipsoids[index,:] = quaternions
+# Angular momentum 
+                  angmom = np.float32(line.split()[13:16]) 
+                  conf.Lv = angmom
 
                   line = f.readline()
 
-              print(conf.xyz,conf.v)
-              system.print_lorenzo_output(configuration_file, topology_file, None, True)
+              print(conf.xyz, conf.v, conf.ellipsoids, conf.Lv)
 
-          line = f.readline()
+
+                 #line = f.readline()
+
+              for n in range(N):
+
+                  cm = conf.xyz[index,:]
+                  quaternions = conf.ellipsoids[index,:]
+                  a1, a3 = quat_to_exyz(quaternions)
+                  b = number_oxdna_to_lammps[conf.bases[i]-1]
+
+                  v = conf.v[index,:]
+                  Lv = conf.Lv[index,:]
+
+                  strands[conf.strand[i]-1].add_nucleotide(base.Nucleotide(cm, a1, a3, b, b, v, Lv))
+                          
+                  # close strand 
+                  next_bond=conf.bonds[i][1]
+                  if next_bond!=-1 and next_bond!=i+1:
+                    if conf.strand[i]!=conf.strand[next_bond]:
+                      print("Wrong bond arising between two different strands", file=sys.stderr)
+                    else:
+                      strands[conf.strand[i]-1].make_circular()
+
+
+
+
+              #print(conf.xyz, conf.v, conf.ellipsoids, conf.Lv)
+
+
+              #line = f.readline()
+
+
+          system.print_lorenzo_output(configuration_file, topology_file, None, True)
+
+        line = f.readline()
 
 
     print("## Wrote data to '%s' / '%s'" % (configuration_file, topology_file), file=sys.stderr)
