cafd832078c1b71e1c1137a45be349f508f8ed0a
elija feigl <elija-feigl@users.noreply.github.com>
2021-11-02 15:00:43 +0100

    oxDNA_PDB.py: RMSF data (#34)
    Add a new feature to the oxDNA -> PDB converter
    
    Add the `--rmsf-file` option to add bfactor .pdb bases on rmsf data compatible with the sulcgroup/oxdna_analysis_tools compute_deviations tool

--- src/oxDNA_PDB.py
+++ src/oxDNA_PDB.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 import copy
 from math import sqrt, sin
+from collections import defaultdict
 
 from libs.pdb import Atom, Nucleotide, FROM_OXDNA_TO_ANGSTROM
 from libs import base
@@ -14,24 +15,25 @@ from libs.readers import LorenzoReader
 DD12_PDB_PATH = "dd12_na.pdb"
 
 def print_usage():
-        print("USAGE:", file=sys.stderr)
-        print("\t%s topology configuration direction" % sys.argv[0], file=sys.stderr)
-        print("\t[-H\--hydrogens=True] [-u\--uniform-residue-names] [-o\--one-file-per-strand]", file=sys.stderr)
-        exit(1)
+    print("USAGE:", file=sys.stderr)
+    print("\t%s topology configuration direction" % sys.argv[0], file=sys.stderr)
+    print("\t[-H\--hydrogens=True] [-u\--uniform-residue-names] [-o\--one-file-per-strand] [--rmsf-file]", file=sys.stderr)
+    exit(1)
 
 def parse_options():
     shortArgs = 'H:uo'
-    longArgs = ['hydrogens=','uniform-residue-names', 'one-file-per-strand']
-    
+    longArgs = ['hydrogens=', 'uniform-residue-names', 'one-file-per-strand', 'rmsf-file=']
+
     opts = {
         "configuration" : "",
         "topology" : "",
         "oxDNA_direction" : True,
         "print_hydrogens" : True,
         "uniform_residue_names" : False,
-        "one_file_per_strand" : False
+        "one_file_per_strand" : False,
+        "rmsf_bfactor" : "",
     }
-    
+
     try:
         import getopt
         args, positional_args = getopt.gnu_getopt(sys.argv[1:], shortArgs, longArgs)
@@ -47,14 +49,16 @@ def parse_options():
                     print("The argument of '%s' should be either 'true' or 'false' (got '%s' instead)" % (k[0], k[1]), file=sys.stderr)
                     exit(1)
             elif k[0] == '-u' or k[0] == '--uniform-residue-names':
-                    opts["uniform_residue_names"] = True
+                opts["uniform_residue_names"] = True
             elif k[0] == '-o' or k[0] == '--one-file-per-strand':
-                    opts["one_file_per_strand"] = True
-            
+                opts["one_file_per_strand"] = True
+            elif k[0] == '--rmsf-file':
+                opts["rmsf_bfactor"] = k[1]
+
         opts['topology'] = positional_args[0]
         opts['configuration'] = positional_args[1]
         direction = positional_args[2]
-        
+
         if direction == "35":
             opts["oxDNA_direction"] = True
         elif direction == "53":
@@ -64,28 +68,28 @@ def parse_options():
             exit(1)
     except Exception:
         print_usage()
-        
+
     return opts
 
 def align(full_base, ox_base):
-        theta = utils.get_angle(full_base.a3, ox_base._a3)
-        # if the two bases are already essentially aligned then we do nothing
-        if sin(theta) > 1e-3:
-            axis = np.cross(full_base.a3, ox_base._a3)
-            axis /= sqrt(np.dot(axis, axis))
-            R = utils.get_rotation_matrix(axis, theta)
-            full_base.rotate(R)
-    
-        theta = utils.get_angle(full_base.a1, ox_base._a1)
-        if sin(theta) > 1e-3:
-            axis = np.cross(full_base.a1, ox_base._a1)
-            axis /= sqrt(np.dot(axis, axis))
-            R = utils.get_rotation_matrix(axis, theta)
-            full_base.rotate(R)
+    theta = utils.get_angle(full_base.a3, ox_base._a3)
+    # if the two bases are already essentially aligned then we do nothing
+    if sin(theta) > 1e-3:
+        axis = np.cross(full_base.a3, ox_base._a3)
+        axis /= sqrt(np.dot(axis, axis))
+        R = utils.get_rotation_matrix(axis, theta)
+        full_base.rotate(R)
+
+    theta = utils.get_angle(full_base.a1, ox_base._a1)
+    if sin(theta) > 1e-3:
+        axis = np.cross(full_base.a1, ox_base._a1)
+        axis /= sqrt(np.dot(axis, axis))
+        R = utils.get_rotation_matrix(axis, theta)
+        full_base.rotate(R)
 
 if __name__ == '__main__':
     opts = parse_options()
-        
+
     with open(os.path.join(os.path.dirname(__file__), DD12_PDB_PATH)) as f:
         nucleotides = []
         old_residue = ""
@@ -103,40 +107,58 @@ if __name__ == '__main__':
         n.compute_as()
         if n.base in bases:
             if n.check < bases[n.base].check: bases[n.base] = copy.deepcopy(n)
-        else: 
+        else:
             bases[n.base] = n
-    
+
     for n in nucleotides:
         n.a1, n.a2, n.a3 = utils.get_orthonormalized_base(n.a1, n.a2, n.a3)
-    
+
     try:
         lr = LorenzoReader(opts['topology'], opts['configuration'])
         s = lr.get_system()
     except Exception as e:
         print("Parser error: %s" % e, file=sys.stderr)
         exit(1)
-    
+
+    rmsf_file = opts["rmsf_bfactor"]
+    if rmsf_file:
+        with open(rmsf_file) as f:
+            try:
+                # https://github.com/sulcgroup/oxdna_analysis_tools
+                substrings = f.read().split("[")[1].split("]")[0].split(",")
+            except Exception as e:
+                print("Parsing error in RMSF file. Invalid Format: %s" % e, file=sys.stderr)
+                exit(1)
+            try:
+                rmsf_per_nucleotide = {i: float(s)
+                                       for i, s in enumerate(substrings)}
+            except Exception as e:
+                print("Parsing error in RMSF file. Conversion to float failed : %s" % e, file=sys.stderr)
+                exit(1)
+    else:
+        rmsf_per_nucleotide = defaultdict(lambda: 1.00)
+
     ox_nucleotides = []
     s.map_nucleotides_to_strands()
     com = np.array([0., 0., 0.])
     for my_strand in s._strands:
         com += my_strand.cm_pos
     com /= s.N_strands
-    
+
     box_angstrom = s._box * FROM_OXDNA_TO_ANGSTROM
     correct_for_large_boxes = False
     if np.any(box_angstrom[box_angstrom > 999]):
         print("At least one of the box sizes is larger than 999: all the atoms which are outside of the box will be brought back through periodic boundary conditions", file=sys.stderr)
         correct_for_large_boxes = True
-    
+
     if opts['one_file_per_strand']:
         out_name = opts['configuration'] + "_1.pdb"
     else:
         out_name = opts['configuration'] + ".pdb"
-        
+
     out = open(out_name, "w")
-    
-    current_base_identifier = 'A'    
+
+    current_base_identifier = 'A'
     for s_id, strand in enumerate(s._strands):
         strand_pdb = []
         nucleotides_in_strand = strand._nucleotides
@@ -147,15 +169,15 @@ if __name__ == '__main__':
             my_base = copy.deepcopy(bases[nb])
             my_base.chain_id = s._nucleotide_to_strand[nucleotide.index]
             residue_type = ""
-            
+
             if not strand.is_circular():
                 # 3' end
                 if nucleotide == strand._nucleotides[0]:
                     residue_type = "3"
                 # 5' end
                 elif nucleotide == strand._nucleotides[-1]:
-                    residue_type = "5" 
-                
+                    residue_type = "5"
+
             if opts["uniform_residue_names"] == True:
                 residue_suffix = ""
             else:
@@ -166,16 +188,17 @@ if __name__ == '__main__':
 
             if correct_for_large_boxes:
                 my_base.correct_for_large_boxes(box_angstrom)
-            
+
             residue_serial = n_idx % 9999
             base_identifier = current_base_identifier
-            nucleotide_pdb = my_base.to_pdb(base_identifier, opts['print_hydrogens'], residue_serial, residue_suffix, residue_type)
+            nucleotide_pdb = my_base.to_pdb(base_identifier, opts['print_hydrogens'], residue_serial, residue_suffix,
+                                            residue_type, bfactor=rmsf_per_nucleotide[nucleotide.index])
             strand_pdb.append(nucleotide_pdb)
-            
-                
+
+
         print("\n".join(x for x in strand_pdb), file=out)
         print("TER", file=out)
-        
+
         if opts['one_file_per_strand']:
             out.close()
             print("## Wrote strand %d's data to '%s'" % (s_id + 1, out_name), file=sys.stderr)
@@ -189,9 +212,9 @@ if __name__ == '__main__':
                 current_base_identifier = 'A'
             else:
                 current_base_identifier = chr(ord(current_base_identifier) + 1)
-    
+
     if not opts['one_file_per_strand']:
         out.close()
         print("## Wrote data to '%s'" % out_name, file=sys.stderr)
-        
+
     print("## DONE", file=sys.stderr)
